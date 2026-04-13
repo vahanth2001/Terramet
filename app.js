e735dbcb383514545d6cd839ba20364da12a8fcc
guna <vahanthguna765992@gmail.com>
2026-04-13 13:10:03 +0530

    22

--- app.js
+++ app.js
@@ -25,3 +25,55 @@ document.addEventListener("DOMContentLoaded", function () {
   hero.addEventListener("mouseenter", stopSlider);
   hero.addEventListener("mouseleave", startSlider);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+
+  // HERO SLIDER
+  const slides = document.querySelectorAll(".slide");
+  let index = 0;
+  let interval;
+
+  function showSlide() {
+    slides.forEach(slide => slide.classList.remove("active"));
+    slides[index].classList.add("active");
+    index = (index + 1) % slides.length;
+  }
+
+  function startSlider() {
+    interval = setInterval(showSlide, 4000);
+  }
+
+  function stopSlider() {
+    clearInterval(interval);
+  }
+
+  startSlider();
+
+  const hero = document.querySelector(".hero");
+  hero.addEventListener("mouseenter", stopSlider);
+  hero.addEventListener("mouseleave", startSlider);
+
+
+  // HAMBURGER MENU
+  const hamburger = document.getElementById("hamburger");
+  const navMenu = document.getElementById("nav-menu");
+
+  hamburger.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+  });
+
+
+  // MOBILE DROPDOWN
+  const dropdowns = document.querySelectorAll(".dropdown > a");
+
+  dropdowns.forEach(item => {
+    item.addEventListener("click", function (e) {
+      if (window.innerWidth <= 992) {
+        e.preventDefault();
+        const submenu = this.nextElementSibling;
+        submenu.classList.toggle("show");
+      }
+    });
+  });
+
+});
